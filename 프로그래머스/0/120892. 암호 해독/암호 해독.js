00a79e835ec5f493c96a807a0051de1eac724a75
JS_KIM <86825671+GinGer-JSK@users.noreply.github.com>
2024-06-26 21:46:27 +0900

    [level 0] Title: 암호 해독, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120892. 암호 해독/암호 해독.js
+++ 프로그래머스/0/120892. 암호 해독/암호 해독.js
@@ -1,8 +1,4 @@
 function solution(cipher, code) {
-    let result = '';
-    // code - 1부터 시작해서 code씩 증가
-    for (let i = code - 1; i < cipher.length; i += code) {
-        result += cipher[i];
-    }
-    return result;
+    var answer = '';
+    return answer;
 }
